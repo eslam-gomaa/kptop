f6b95f4949ecc3d21d02840ebea1c203c509a861
eslam-gomaa <eslam.sa3dany2015@gmail.com>
2022-12-16 23:13:17 +0200

    update ENVs names

--- kubePtop/read_env.py
+++ kubePtop/read_env.py
@@ -12,28 +12,28 @@ class ReadEnv:
 
         # Mandatory ENV
         try:
-            GlobalAttrs.env_prometheus_server  = os.environ['KUBE_PTOP_PROMETHEUS_SERVER']
+            GlobalAttrs.env_prometheus_server  = os.environ['KPTOP_PROMETHEUS_SERVER']
         except (KeyError) as e:
             raise SystemExit(f"\nERROR -- ENV not found => {e}")
 
         # Optional ENV
         try:
-            GlobalAttrs.env_basic_auth_enabled  = os.environ['KUBE_PTOP_BASIC_AUTH_ENABLED']
-            GlobalAttrs.env_prometheus_username  = os.environ['KUBE_PTOP_PROMETHEUS_USERNAME']
-            GlobalAttrs.env_prometheus_password  = os.environ['KUBE_PTOP_PROMETHEUS_PASSWORD']
-            GlobalAttrs.env_insecure  = os.environ['KUBE_PTOP_INSECURE']
+            GlobalAttrs.env_basic_auth_enabled  = os.environ['KPTOP_BASIC_AUTH_ENABLED']
+            GlobalAttrs.env_prometheus_username  = os.environ['KPTOP_PROMETHEUS_USERNAME']
+            GlobalAttrs.env_prometheus_password  = os.environ['KPTOP_PROMETHEUS_PASSWORD']
+            GlobalAttrs.env_insecure  = os.environ['KPTOP_INSECURE']
         except (KeyError) as e:
             pass
 
         if GlobalAttrs.env_basic_auth_enabled not in [True, False, 'true', 'false']:
-            print("INFO -- KUBE_PTOP_BASIC_AUTH_ENABLED > allowed options are: 'true' || 'false'")
+            print("INFO -- KPTOP_BASIC_AUTH_ENABLED > allowed options are: 'true' || 'false'")
             exit(1)
 
         if GlobalAttrs.env_insecure not in [True, False, 'true', 'false']:
-            print("INFO -- KUBE_PTOP_INSECURE > allowed options are: 'true' || 'false'")
+            print("INFO -- KPTOP_INSECURE > allowed options are: 'true' || 'false'")
             exit(1)
 
 
         if GlobalAttrs.env_basic_auth_enabled:
             if (GlobalAttrs.env_prometheus_username is None or GlobalAttrs.env_prometheus_password is None):
-                raise SystemExit("INFO -- ENV: KUBE_PTOP_PROMETHEUS_USERNAME or KUBE_PTOP_PROMETHEUS_PASSWORD is missing")
+                raise SystemExit("INFO -- ENV: KPTOP_PROMETHEUS_USERNAME or KPTOP_PROMETHEUS_PASSWORD is missing")
