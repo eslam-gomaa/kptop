103fea885986cbe6d6b1967dfdb29ce0b22716f5
eslam-gomaa <eslam.sa3dany2015@gmail.com>
2022-12-16 23:11:57 +0200

    add

---
+++ kubePtop/colors.py
@@ -0,0 +1,13 @@
+
+class Bcolors:
+    def __init__(self):
+        self.HEADER = '\033[95m'
+        self.OKBLUE = '\033[94m'
+        self.OKGREEN = '\033[92m'
+        self.WARNING = '\033[93m'
+        self.FAIL = '\033[91m'
+        self.ENDC = '\033[0m'
+        self.BOLD = '\033[1m'
+        self.UNDERLINE = '\033[4m'
+        self.GRAY = "\033[1;30;40m"
+        self.GRAY = "\033[90m"
